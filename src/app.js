38ae9f669b4c316c4baddae6a21927dfd05f1f21
METADIL MÓVEIS - Caio Lesnock <caio@metadil.com.br>
2020-11-27 14:48:24 -0300

    Add createAdminUser

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import cors from 'cors'
 import express from 'express'
 
+import User from './models/User'
+
 // Routes
 import { Public, Private } from './routes'
 
@@ -8,6 +10,7 @@ class App {
   constructor() {
     this.server = express()
 
+    this.createAdminUser()
     this.middlewares()
     this.routes()
   }
@@ -23,6 +26,17 @@ class App {
     this.server.use(cors())
     this.server.use(express.urlencoded({ extended: false }))
     this.server.use(express.json())
+  }
+
+  async createAdminUser() {
+    if (!(await User.exists({ username: 'admin' }))) {
+      await User.insert({
+        name: 'Administrador',
+        email: 'ti@metadil.com.br',
+        username: 'admin',
+        password: 'Admin$uporte'
+      })
+    }
   }
 }
 
