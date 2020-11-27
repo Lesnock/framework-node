0e7046c97886d7a566a0ceeb78283a1c9c36fc35
METADIL MÓVEIS - Caio Lesnock <caio@metadil.com.br>
2020-11-27 12:35:50 -0300

    Add babel to eslint

--- src/controllers/MainController.js
+++
@@ -1,9 +0,0 @@
-import Controller from '../core/Controller'
-
-class MainController extends Controller {
-  async index(req, res) {
-    res.send('OK')
-  }
-}
-
-export default new MainController()
